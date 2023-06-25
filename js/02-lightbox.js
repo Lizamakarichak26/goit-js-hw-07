1df54b8cbe5bd97d89b4afb812bba7ccabb20dab
LizaM <liza.makarichak2019@gmail.com>
2023-06-25 19:39:08 +0300

    goit-js-hw-07

---
+++ js/02-lightbox.js
@@ -0,0 +1,16 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems);
+const galleryListElem = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) =>
+`<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`);
+galleryListElem.insertAdjacentHTML('beforeend', markup.join(''));
+let galleryShow = new SimpleLightbox('.gallery a', { captions: true, captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom'});
